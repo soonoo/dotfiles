a526d57bd613b808e3684d87e1a29a4a6d69954c
soonoo <qpseh2m7@gmail.com>
2018-06-10 07:31:28 +0900

    add vim PluginInstall

--- install.sh
+++ install.sh
@@ -5,11 +5,12 @@ sudo apt-get update
 sudo apt-get install vim
 
 # zsh, oh-my-zsh 
-apt install zsh
+sudo apt install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # install Vim Vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
 ln -sf --backup $PWD/.zshrc ~/.zshrc
 
 # install  node.js
