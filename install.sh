a1b5e4771cb5f9a60aa75de85719b6a678aa5a7a
soonoo <qpseh2m7@gmail.com>
2018-06-10 07:11:59 +0900

    add node install

--- install.sh
+++ install.sh
@@ -12,3 +12,6 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln -sf --backup $PWD/.zshrc ~/.zshrc
 
+# install  node.js
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get install -y nodejs
