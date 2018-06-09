e23eec7d5a65f48a1a07d8fe9064e9897d0b3eb2
soonoo <qpseh2m7@gmail.com>
2018-06-10 05:42:50 +0900

    add zsh, vundle install

--- install.sh
+++ install.sh
@@ -1,6 +1,14 @@
 ln -sf --backup $PWD/.vimrc ~/.vimrc
 
+# vim update 
+sudo apt-get update
+sudo apt-get install vim
+
+# zsh, oh-my-zsh 
 apt install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# install Vim Vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln -sf --backup $PWD/.zshrc ~/.zshrc
 
