4c844faade1ec95df664b06d8d084646d449af57
soonoo <qpseh2m7@gmail.com>
2019-05-09 04:20:34 +0900

    add xrandr setup script with dell p2717h
    usage: ./p2717h.sh

---
+++ screenlayout/p2717h.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+xrandr --output eDP1 --primary --mode 2560x1440 --pos 0x1440 --rotate normal \
+  --output DP1 --off --output DP2 --off --output HDMI1 --off --output \
+  HDMI2 --mode 1920x1080 --pos 0x0 --scale 1.333x1.333 --rotate normal --output VIRTUAL1 --off
