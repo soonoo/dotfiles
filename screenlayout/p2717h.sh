60904e7b9ad11b51290719e6db49f8fa686e4786
soonoo <qpseh2m7@gmail.com>
2019-06-29 23:57:31 +0900

    remove mouse flickering when xrandr --scale option is enabled

--- screenlayout/p2717h.sh
+++ screenlayout/p2717h.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-xrandr --output eDP1 --primary --mode 2560x1440 --pos 0x1440 --rotate normal \
-  --output HDMI2 --mode 1920x1080 --pos 0x0 --scale 1.333x1.333 --rotate normal
+xrandr --output eDP-1 --primary --mode 2560x1440 --pos 0x1440 --rotate normal \
+  --output HDMI-2 --mode 1920x1080 --pos 0x0 --scale 1.333x1.333 --rotate normal
+
+# removes cursor flickering
+# https://gitlab.freedesktop.org/xorg/xserver/issues/70
+xrandr --output eDP-1 --scale 0.9999x0.9999
